286befb2b5338d135de5f39c651b7cadf33e78fc
g-pine <pinogallegos.gabriel@gmail.com>
2025-07-29 17:11:50 -0600

    Add auth

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,8 @@
-'use client';
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter"
 import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
+import { useEffect } from "react";
 import NavBar from "~/components/NavBar";
 import ResumeCard from "~/components/ResumeCard";
 
@@ -12,8 +14,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
   return <main className="bg-[url('images/bg-main.svg')]">
     <NavBar />
+    {/* {window.puter.ai.chat()} */}
     <section className="main-section">
       <h1>Track Your Application & Resume Ratings</h1>
       <h2>Review your submissions and check AI-powered feedback</h2>
